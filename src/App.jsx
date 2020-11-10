e76239a4c39457c8ce991da6a48272b55961bf62
Francois Vieux <francois.g.vieux@gmail.com>
2020-11-10 11:28:15 +0100

    done with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 
-import F181A529AD360019AA74744B862D78C3 from './Auth/F181A529AD360019AA74744B862D78C3.txt'
 import MainNav from './components/MainNav';
 
 import { Switch, Route } from "react-router-dom";
@@ -16,8 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Projects} />
-        <Route exact path="/about-me" component={About} />
-        <Route exact path="/.well-known/pki-validation/F181A529AD360019AA74744B862D78C3.txt" component={F181A529AD360019AA74744B862D78C3} />
+        <Route exact path="/articles" component={About} />
       </Switch>
     </div>
   );
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom';
 
 const MainNav = () => {
     return (
-        <div>
-            <NavLink to='/'><h1>François_Georges_Vieux_</h1></NavLink>
-            <ul>
+        <div className='main-nav'>
+            <NavLink to='/home'><h1 className="main-color logo">F_v<span className="orange">_</span></h1></NavLink>
+            <ul className='nav-list'>
                 <li><NavLink to='/projects'>Projects_</NavLink></li>
                 <li><NavLink to='/articles'>Articles_</NavLink></li>
-                <li><NavLink to='/about-me'>About_Me_</NavLink></li>
+                <li><NavLink to='/'>About_Me_</NavLink></li>
             </ul>
         </div>
     )
